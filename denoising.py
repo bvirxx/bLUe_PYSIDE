7a63558e8e38f47e93e86990241ee851516512b6
Bernard <bernard.virot@libertysurf.fr>
2018-10-18 17:49:28 +0200

    added infos

--- denoising.py
+++ denoising.py
@@ -1,3 +1,26 @@
+"""
+This File is part of bLUe software.
+
+Copyright (C) 2017  Bernard Virot <bernard.virot@libertysurf.fr>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Lesser Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+Credit : Michael Beyeler
+http://www.askaswiss.com/2016/12/how-to-denoise-images-in-python.html
+LICENSE CC-BY 4.0 Cf. https://creativecommons.org/licenses/by/4.0/
+"""
 
 import numpy as np
 
