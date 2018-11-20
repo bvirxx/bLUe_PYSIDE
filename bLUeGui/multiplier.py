fixed normalization of as shot multipliers for nef files development default settings set to apply camera profile

bLUeGui/multiplier.py
@@ -27,8 +27,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 
-from dng import dngProfileIlluminants, dngProfileColorMatrices, interpolate, dngProfileDual, interpolatedColorMatrix
+from dng import interpolate, dngProfileDual, interpolatedColorMatrix
 from .colorCIE import temperatureAndTint2xy, temperature2xyWP
+
 
 def CIExyY2XYZ(x, y):
     """
@@ -44,6 +45,7 @@ def CIExyY2XYZ(x, y):
     """
     return x / y, 1, (1 - x - y) / y
 
+
 def XYZ2CIExyY(X, Y, Z):
     """
     Return the two first coordinates
@@ -55,15 +57,17 @@ def XYZ2CIExyY(X, Y, Z):
     return X / s, Y / s
 
 
-def temperatureAndTint2RGBMultipliers(temp, tint, XYZ2CameraMatrix, dngDict={}):
+def temperatureAndTint2Multipliers(temp, tint, XYZ2CameraMatrix, dngDict={}):
     """
     Convert temperature and tint to RGB multipliers and apply a
-    tint shift : mG = WP_G * tint.
-    The matrix XYZ2CameraMatrix should the interpolated Camera Color Matrix for the
-    temperature T.
+    tint shift : mG = WP_G * tint. The conversion algorithm is based on the
+    camera profile matrix CM for the temperature T.
+    If dngDict is a valid dual illuminant profile, CM is the interpolated Camera Color Matrix for
+    temperature T, and the parameter XYZ2CameraMatrix is not used.
+    If dngDict is invalid, then CM = XYZ2CameraMatrix.
     We compute the xy coordinates of the white point WP(T) by the Robertson's method.
     Next, we transform these coordinates to camera RGB values (mR,mG,mB), using the
-    conversion matrix XYZ2RGBMatrix.
+    conversion matrix CM.
     Multipliers are m1 = mR, m2 = mG*tint, m3 = mB. For convenience
     the function returns the 4 values m1, m2, m3, m2, scaled to min(m1,m2,m3)=1.
     The tint factor should be between 0.2 and 2.5
@@ -93,31 +97,34 @@ def temperatureAndTint2RGBMultipliers(temp, tint, XYZ2CameraMatrix, dngDict={}):
     # expand to XYZ color space
     WP_X, WP_Y, WP_Z = CIExyY2XYZ(WP_x, WP_y)  # x / y, 1.0, (1.0 - x - y) / y
     # Convert to camera neutral
-    m1, m2, m3 = np.dot(colorMatrix, [WP_X, WP_Y, WP_Z]) # np.dot(XYZ2CameraMatrix, [WP_X, WP_Y, WP_Z])
+    m1, m2, m3 = np.dot(colorMatrix, [WP_X, WP_Y, WP_Z])  # np.dot(XYZ2CameraMatrix, [WP_X, WP_Y, WP_Z])
     # apply tint shift (green-magenta shift) to G channel.
     m2 = m2 * tint
     mi = min((m1, m2, m3))
     m1, m2, m3 = m1 / mi, m2 / mi, m3 / mi
     return m1, m2, m3, m2
 
-def RGBMultipliers2TemperatureAndTint(mR, mG, mB, XYZ2CameraMatrix, dngDict={}):
+
+def multipliers2TemperatureAndTint(mR, mG, mB, XYZ2CameraMatrix, dngDict={}):
     """
     Inverse function of temperatureAndTint2RGBMultipliers.
-    Compute the temperature and tint correction corresponding to a
+    It calculates the CCT temperature and the tint correction corresponding to a
     set of 3 RGB multipliers.
-    The matrix XYZ2CameraMatrix should the interpolated Camera Color Matrix for the
-    temperature T.
+    The conversion algorithm is based on the
+    camera profile matrix CM for the temperature T.
+    If dngDict is a valid dual illuminant profile, CM is the interpolated Camera Color Matrix for
+    temperature T, and the parameter XYZ2CameraMatrix is not used.
+    If dngDict is invalid, then CM = XYZ2CameraMatrix.
     The aim is to find a temperature T with a corresponding white point WP(T),
     and a factor tint, such that mB/mR = WPb/WPr and mG*tint/mR = Wpg/WPr.
     We consider the function f(T) = WPb/WPr giving
     the ratio of blue over red coordinates for the white point WP(T). Assuming  f is monotonic,
     we solve the equation f(T) = mB/mR by a simple dichotomous search.
     The Adobe dng spec. uses a slightly different algorithm (cf. p. 81) : they search for a white point
-    point (x,y) = WP(T) which is solution of the equation XYZtoCamera(WP(T)) = CameraNeutral,
-    with XYZtoCamera interpolated from ColorMatrix1 and ColorMatrix2.
-    Then, the tint is simply defined as the scaling factor mu verifying tint * mG/mR = WPG/WPR
+    point (x,y) = WP(T) which is solution of the equation CM(WP(T)) = CameraNeutral,
+    Tint is simply defined as the scaling factor mu verifying tint * mG/mR = WPG/WPR
     Note that to be inverse functions, RGBMultipliers2Temperature and temperatureAndTint2RGBMultipliers
-    must use the same XYZ2CameraMatrix.
+    must use the same conversion matrix.
     @param mR:
     @type mR:
     @param mG:
@@ -126,7 +133,7 @@ def RGBMultipliers2TemperatureAndTint(mR, mG, mB, XYZ2CameraMatrix, dngDict={}):
     @type mB:
     @param XYZ2CameraMatrix:
     @type XYZ2CameraMatrix:
-     @param dngDict: dictionary of dng profile tag values
+    @param dngDict: dictionary of dng profile tag values
     @type dngDict: dict
     @return: temperature and tint correction
     @rtype: 2-uple of float
@@ -144,7 +151,7 @@ def RGBMultipliers2TemperatureAndTint(mR, mG, mB, XYZ2CameraMatrix, dngDict={}):
         T = (Tmin + Tmax) / 2.0
         x, y = temperature2xyWP(T)  # ~ temperatureAndTint2xy(T,0)
         # expand to the XYZ color space
-        X, Y, Z = CIExyY2XYZ(x, y) #= x / y, 1, (1 - x - y) / y
+        X, Y, Z = CIExyY2XYZ(x, y)  # x / y, 1, (1 - x - y) / y
         # Convert to camera color space :
         if dualIlluminant:
             M = interpolate(T, colorMatrix1, colorMatrix2, T1, T2)

graphicsRaw.py
@@ -172,12 +172,11 @@ class rawForm (baseForm):
         # constants and as shot values
         self.XYZ2CameraMatrix = rawpyObj.rgb_xyz_matrix[:3, :]
         self.XYZ2CameraInverseMatrix = np.linalg.inv(self.XYZ2CameraMatrix)
-        self.asShotMultipliers = rawpyObj.camera_whitebalance
-        self.asShotTemp, self.asShotTint = multipliers2TemperatureAndTint(*1 / np.array(self.asShotMultipliers[:3]), self.XYZ2CameraMatrix)
-
-
         # initial post processing multipliers (as shot)
-        self.rawMultipliers = rawpyObj.camera_whitebalance # = 1/(dng ASSHOTNEUTRAL tag value)
+        m1,m2,m3, m4 = rawpyObj.camera_whitebalance
+        self.asShotMultipliers = (m1/m2, 1.0, m3/m2, m4/m2) # normalization is mandatory : for nef files white balance is around 256
+        self.asShotTemp, self.asShotTint = multipliers2TemperatureAndTint(*1 / np.array(self.asShotMultipliers[:3]), self.XYZ2CameraMatrix)
+        self.rawMultipliers = self.asShotMultipliers #rawpyObj.camera_whitebalance # = 1/(dng ASSHOTNEUTRAL tag value)
         self.sampleMultipliers = False
         self.samples = []
         ########################################
@@ -788,7 +787,7 @@ former.<br>
         items = OrderedDict(
             [(basename(f)[:-4] if i > 0 else 'Embedded Profile', getDngProfileDict(f)) for i, f in enumerate(files)])
         # add 'None' and all found profiles for the current camera model: 'None' will be the default selection
-        self.cameraProfilesCombo.addItem('None', {})
+
         # filter items to eliminate empty entries and
         # add non empty dicts to cameraProfileCombo
         for key in items:
@@ -796,6 +795,7 @@ former.<br>
             d = {k: items[key][k] for k in items[key] if items[key][k] != ''}
             if d:
                 self.cameraProfilesCombo.addItem(key, d)
+        self.cameraProfilesCombo.addItem('None', {})
         self.cameraProfilesCombo.setSizeAdjustPolicy(QComboBox.SizeAdjustPolicy.AdjustToContents)
         self.cameraProfilesCombo.setMaximumWidth(150)
         self.cameraProfilesCombo.setStyleSheet("QComboBox QAbstractItemView { min-width: 250px;}")

rawProcessing.py
@@ -25,7 +25,8 @@ from PySide2.QtGui import QImage
 
 from bLUeCore.multi import interpMulti
 from bLUeGui.bLUeImage import QImageBuffer, bImage
-from bLUeGui.colorCIE import rgbLinear2rgbVec, sRGB_lin2XYZ, sRGB_lin2XYZInverse, bradfordAdaptationMatrix
+from bLUeGui.colorCIE import rgbLinear2rgbVec, sRGB_lin2XYZ, sRGB_lin2XYZInverse, bradfordAdaptationMatrix, \
+    sRGB_lin2XYZ_D50Inverse
 from bLUeGui.graphicsSpline import channelValues
 from bLUeGui.histogramWarping import warpHistogram
 from debug import tdec
@@ -101,6 +102,7 @@ def rawPostProcess(rawLayer, pool=None):
 
     use_auto_wb = options['Auto WB']
     use_camera_wb = options['Camera WB']
+    exp_preserve_highlights = 0.99 if options['Preserve Highlights'] else 0.2  # 0.6  # range 0.0..1.0 (1.0 = full preservation)
     if doALL:
         ##############################
         # get postprocessing parameters
@@ -111,7 +113,7 @@ def rawPostProcess(rawLayer, pool=None):
         exp_shift = adjustForm.expCorrection if not options['Auto Brightness'] else 0
         no_auto_bright = (not options['Auto Brightness'])
 
-        exp_preserve_highlights = 0.99 if options['Preserve Highlights'] else 0.6  # range 0.0..1.0
+
         bright = adjustForm.brCorrection  # default 1, should be > 0
         hv = adjustForm.overexpValue
         highlightmode = rawpy.HighlightMode.Clip if hv == 0 \
@@ -191,32 +193,19 @@ def rawPostProcess(rawLayer, pool=None):
     D = np.diag((1/m1,1/m2,1/m3))
     tempCorrection = adjustForm.asShotTemp if use_camera_wb else adjustForm.tempCorrection
     MM = bradfordAdaptationMatrix(6500, tempCorrection)
-    FM, BM = None, None
+    MM1 = bradfordAdaptationMatrix(6500, 5000)
+    FM = None
+    myHighlightPreservation = 0.8 if exp_preserve_highlights > 0.9 else 1.0
     if adjustForm.dngDict:
         try:
-            #BM = interpolatedColorMatrix(adjustForm.tempCorrection, adjustForm.dngDict)
-            #BMInverse = np.linalg(BM)
             FM = interpolatedForwardMatrix(adjustForm.tempCorrection, adjustForm.dngDict)
         except:
             pass
-    raw2sRGBMatrix = sRGB_lin2XYZInverse @ FM if FM is not None else sRGB_lin2XYZInverse @ MM @ adjustForm.XYZ2CameraInverseMatrix @ D #  @ MMInverse
+    raw2sRGBMatrix = sRGB_lin2XYZInverse @ MM1 @ FM * myHighlightPreservation if FM is not None else\
+                     sRGB_lin2XYZ_D50Inverse @ MM @ adjustForm.XYZ2CameraInverseMatrix @ D
     bufpost16 = np.tensordot(rawLayer.bufpost16, raw2sRGBMatrix, axes=(-1, -1))
     np.clip(bufpost16, 0, 255, out=bufpost16)
     rawLayer.postProcessCache = cv2.cvtColor(((bufpost16.astype(np.float32)) / max_ouput).astype(np.float32), cv2.COLOR_RGB2HSV)
-    """
-    try:
-        if adjustForm.dngDict:
-            M = interpolatedColorMatrix(adjustForm.tempCorrection, adjustForm.dngDict)
-            M1 = np.linalg.inv(M)
-            #bufpost16 = np.tensordot(rawLayer.bufpost16, sRGB_lin2XYZInverse @ M1 @ adjustForm.XYZ2CameraMatrix , axes=(-1, -1))
-            #bufpost16 = np.tensordot(rawLayer.bufpost16, sRGB_lin2XYZInverse @ FM @ D @ adjustForm.XYZ2CameraMatrix @ MMInverse, axes=(-1, -1))  # for raw output
-            bufpost16 = np.tensordot(rawLayer.bufpost16, sRGB_lin2XYZInverse @ FM, axes=(-1, -1))
-        else:
-            raise ValueError
-    except (KeyError, ValueError):
-        pass
-        #bufpost16 = np.tensordot(rawLayer.bufpost16, FM @ D @  adjustForm.XYZ2CameraMatrix  @ MMInverse, axes=(-1, -1))  # for raw output
-    """
 
     # update histogram
     s = rawLayer.postProcessCache.shape
@@ -226,18 +215,6 @@ def rawPostProcess(rawLayer, pool=None):
     rawLayer.linearImg = tmp
 
     if getattr(adjustForm, "toneForm", None) is not None:
-        """
-        if doALL or toneCurveShowFirst:
-            # update histogram
-            s = rawLayer.postProcessCache.shape
-            tmp = bImage(s[1], s[0], QImage.Format_RGB32)
-            buf = QImageBuffer(tmp)
-            buf[:, :, :] = (rawLayer.postProcessCache[:, :, 2, np.newaxis] * 255).astype(np.uint8)
-            rawLayer.linearImg = tmp
-            rawLayer.histImg = tmp.histogram(size=adjustForm.toneForm.scene().axeSize,
-                                             bgColor=adjustForm.toneForm.scene().bgColor,
-                                             range=(0, 255), chans=channelValues.Br) # mode='Luminosity')
-        """
         rawLayer.histImg = tmp.histogram(size=adjustForm.toneForm.scene().axeSize,
                                          bgColor=adjustForm.toneForm.scene().bgColor,
                                          range=(0, 255), chans=channelValues.Br)  # mode='Luminosity')
@@ -247,7 +224,6 @@ def rawPostProcess(rawLayer, pool=None):
     # beginning of the camera profile phase : update buffers from the last post processed image
     bufHSV_CV32 = rawLayer.postProcessCache.copy()
     rawLayer.bufCache_HSV_CV32 = bufHSV_CV32.copy()
-
 
     ##########################
     # Profile look table