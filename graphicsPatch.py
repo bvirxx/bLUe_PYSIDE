a455352f7481534b92ec560de3e31309f97adea2
Bernard <bernard.virot@libertysurf.fr>
2018-11-30 16:06:11 +0100

    Use semi transparent color masks

--- graphicsPatch.py
+++ graphicsPatch.py
@@ -54,8 +54,8 @@ class patchForm (baseForm):
         self.layer.cloningMethod = options_dict['Normal Clone']
         self.layer.maskIsEnabled = True
         self.layer.maskIsSelected = True
-        # mask all pixels
-        self.layer.resetMask(maskAll=True)
+        # mask all pixels, use a semi transparent mask
+        self.layer.resetMask(maskAll=True, alpha=128)
         self.layer.autoclone = False
         self.layer.cloningMethod = cv2.NORMAL_CLONE
         self.options={}
@@ -76,18 +76,6 @@ class patchForm (baseForm):
         item = self.listWidget1.items[options[0]]
         item.setCheckState(Qt.Checked)
         self.listWidget1.select(item)
-        """
-        opList2 = ['Auto Cloning', 'Press Button To Clone']
-        self.listWidget2 = optionsWidget(options=opList2)
-        def onSelect2(item):
-            keepCloned = self.listWidget2.options[opList2[0]]
-            self.layer.keepCloned = keepCloned
-            self.layer.maskIsEnabled = not keepCloned
-            self.layer.maskIsSelected = not keepCloned
-        self.listWidget2.onSelect = onSelect2
-        sel = opList2[1]
-        self.listWidget2.select(self.listWidget2.items[sel])
-        """
         pushButton1 = QPushButton('Clone')
         # button clicked event handler
         def f():
@@ -116,91 +104,24 @@ class patchForm (baseForm):
             layer.parentImage.onImageChanged()
         pushButton2.clicked.connect(g)
         # layout
-        l = QVBoxLayout()
-        l.setAlignment(Qt.AlignBottom)
+        layout = QVBoxLayout()
+        layout.setContentsMargins(20, 0, 20, 25)  # left, top, right, bottom
+        self.setLayout(layout)
+        layout.addWidget(self.listWidget1)
         hl = QHBoxLayout()
-        l.addLayout(hl)
-        l.setContentsMargins(20, 0, 20, 25)  # left, top, right, bottom
-        self.setLayout(l)
-        l.addWidget(self.listWidget1)
-        # l.addWidget(self.listWidget2)
-        l.addWidget(pushButton1)
-        l.addWidget(pushButton2)
+        hl.addWidget(pushButton1)
+        hl.addWidget(pushButton2)
+        layout.addLayout(hl)
         self.setWhatsThis(
 """
 <b>Cloning</b> :
 Seamless replacement of a region of the image by another region from the same image (e.g. to erase an object):<br>
    &nbsp; 1) Select the Unmask/FG tool and paint the pixels to erase (use the Mask/BG tool to adjust if needed); <br>
-   &nbsp; 2) Select the drag tool and while pressing Ctrl-Alt, drag or zoom the image shown in the painted region with the mouse; 
+   &nbsp; 2) Select the drag tool and while pressing <b>Ctrl-Alt</b>, drag or zoom the image shown in the painted region with the mouse; 
              the mask can be modified with the mask/unmask tools<br>
    &nbsp; 3) Click the Clone button to start the cloning.<br>
    &nbsp; 4) Redo steps 2 and 3 until the result is satisfactory.<br>
 <b> While executing steps 1 to 4 above, make sure the that layer is the topmost visible layer</b>
 """
                         )
-class maskForm (baseForm):
-    """
-    Knitting form (cloning an imported image)
-    """
-    @classmethod
-    def getNewWindow(cls, targetImage=None, axeSize=200, layer=None, parent=None, mainForm=None):
-        wdgt = maskForm(targetImage=targetImage, axeSize=axeSize, layer=layer, parent=parent, mainForm=mainForm)
-        wdgt.setWindowTitle(layer.name)
-        return wdgt
-
-    def __init__(self, targetImage=None, axeSize=500, layer=None, parent=None, mainForm=None):
-        super().__init__(parent=parent)
-        self.targetImage = targetImage
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        self.setMinimumSize(axeSize, axeSize)
-        self.setAttribute(Qt.WA_DeleteOnClose)
-        self.img = targetImage
-        self.layer = layer
-        self.mainForm = mainForm
-        # options
-        options_dict = {'Normal Clone':cv2.NORMAL_CLONE, 'Mixed Clone':cv2.MIXED_CLONE, 'Monochrome Transfer':cv2.MONOCHROME_TRANSFER}
-        options = list(options_dict.keys())
-        self.layer.cloningMethod = options_dict['Normal Clone']
-        self.options={}
-        for op in options:
-            self.options[op] = False
-        self.listWidget1 = optionsWidget(options=options, exclusive=True)
-        sel = options[0]
-        self.listWidget1.select(self.listWidget1.items[sel])
-        self.options[sel] = True
-        # select event handler
-        def onSelect1(item):
-            for key in self.options:
-                self.options[key] = item is self.listWidget1.items[key]
-                if self.options[key]:
-                    self.layer.cloningMethod = options_dict[key]
-        self.listWidget1.onSelect = onSelect1
-        self.layer.sourceIndex = 0
-        label1 = QLabel('Source')
-        self.sourceCombo = QComboBox()
-        for i, item  in enumerate(self.img.layersStack):
-            self.sourceCombo.addItem(item.name, i)
-        # combo box item chosen event handler
-        def g(ind):
-            self.layer.sourceIndex = ind
-            self.img.getActiveLayer().applyToStack()
-            self.img.onImageChanged()
-        hl = QHBoxLayout()
-        hl.addWidget(label1)
-        hl.addWidget(self.sourceCombo)
-        l = QVBoxLayout()
-        l.setAlignment(Qt.AlignBottom)
-        l.addLayout(hl)
-        l.setContentsMargins(20, 0, 20, 25)  # left, top, right, bottom
-        self.setLayout(l)
-        # set initial selection to normal cloning
-        item = self.listWidget1.items[options[0]]
-        item.setCheckState(Qt.Checked)
-        self.listWidget1.select(item)
-        l.addWidget(self.listWidget1)
-        pushButton1 = QPushButton('Seamless Knit')
-        def f():
-            self.targetImage.getActiveLayer().applyKnitting()
-        pushButton1.clicked.connect(f)
-        l.addWidget(pushButton1)
 
