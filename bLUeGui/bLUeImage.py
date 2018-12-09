0aeebd5e99fe9c4aa21252183f91336ca939f38f
Bernard <bernard.virot@libertysurf.fr>
2018-12-09 11:01:47 +0100

    moved declaration of mask attributes to bImage added methods bImage.fromImage moved method cacheInvalidate to bImage

--- bLUeGui/bLUeImage.py
+++ bLUeGui/bLUeImage.py
@@ -18,25 +18,39 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import cv2
 
 import numpy as np
-from PySide2.QtCore import QSize, QRect, QPoint, Qt, QPointF
+from PySide2.QtCore import QSize, QRect, QPoint, Qt
 from PySide2.QtGui import QImage, QPixmap, QColor, QPainter
 
 from bLUeCore.SavitskyGolay import SavitzkyGolayFilter
 from bLUeGui.graphicsSpline import channelValues
 
+
 class bImage(QImage):
     """
     Base class for all bLUe images.
-    Inherits from QImage and adds a bunch
-    of caches encapsulated as properties.
+    Inherits from QImage. Adds a mask, a pixmap
+    and a bunch of caches encapsulated as properties.
+    The pixmap is synchronized with the image by the
+    method updatePixmap().
     """
+    @classmethod
+    def fromImage(cls, img, parentImage=None):
+        bImg = bImage(img)
+        bImg.parentImage = parentImage
+        return bImg
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.filename = ''
-        self.rPixmap = None
-        self.hspbBuffer = None
-        self.LabBuffer = None
-        self.HSVBuffer = None
+        self.__filename = ''
+        self.__rPixmap = None
+        self.__hspbBuffer = None
+        self.__LabBuffer = None
+        self.__HSVBuffer = None
+        self.maskedImageContainer = None
+        self.maskedThumbContainer = None
+        self.mask = None
+        self.maskIsEnabled = False
+        self.maskIsSelected = False
 
     @property
     def filename(self):
@@ -78,18 +92,39 @@ class bImage(QImage):
     def HSVBuffer(self, buffer):
         self.__HSVBuffer = buffer
 
+    def cacheInvalidate(self):
+        """
+        Invalidate cache buffers.
+        (called by applyToStack after layer.execute)
+        """
+        self.hspbBuffer = None
+        self.LabBuffer = None
+        self.HSVBuffer = None
+        # maskedxxxContainer objects are meant to be
+        # of type bImage, or a subclass of bImage : we
+        # try to invalidate their cache buffers recursively.
+        try:
+            self.maskedImageContainer.cacheInvalidate()
+        except AttributeError:
+            pass
+        try:
+            self.maskedThumbContainer.cacheInvalidate()
+        except AttributeError:
+            pass
+
     def updatePixmap(self, maskOnly=False):
         """
         To respect the Substitutability Principle of Liskov
-        for subtypes, we should keep identical signatures for all
+        for subtypes, we keep identical signatures for all
         overriding methods, so we define here an unused parameter
         maskOnly.
-        @param maskOnly: not used
+        @param maskOnly:
         @type maskOnly: boolean
         """
         self.rPixmap = QPixmap.fromImage(self)
 
-    def histogram(self, size=QSize(200, 200), bgColor=Qt.white, range =(0,255), chans=channelValues.RGB, chanColors=Qt.gray, mode='RGB', addMode=''):
+    def histogram(self, size=QSize(200, 200), bgColor=Qt.white, range=(0, 255),
+                  chans=channelValues.RGB, chanColors=Qt.gray, mode='RGB', addMode=''):
         """
         Plot the image histogram with the
         specified color mode and channels.
@@ -107,6 +142,8 @@ class bImage(QImage):
         @type chanColors: QColor or 3-uple of QColor
         @param mode: color mode ((one among 'RGB', 'HSpB', 'Lab', 'Luminosity')
         @type mode: str
+        @param addMode:
+        @type addMode:
         @return: histogram plot
         @rtype: QImage
         """
@@ -116,16 +153,15 @@ class bImage(QImage):
         # alert threshold for clipped areas
         clipping_threshold = 0.02
         # clipping threshold for black and white points
-        BWP_clipping_threshold = 0.02
         # scaling factor for the bin edges
         spread = float(range[1] - range[0])
         scale = size.width() / spread
 
         # per channel histogram function
         def drawChannelHistogram(painter, hist, bin_edges, color):
-            #Draw the (smoothed) histogram for a single channel.
-            #param painter: QPainter
-            #param hist: histogram to draw
+            # Draw the (smoothed) histogram for a single channel.
+            # param painter: QPainter
+            # param hist: histogram to draw
             # smooth the histogram (first and last bins excepted) for a better visualization of clipping.
             hist = np.concatenate(([hist[0]], SavitzkyGolayFilter.filter(hist[1:-1]), [hist[-1]]))
             M = max(hist[1:-1])
@@ -139,15 +175,16 @@ class bImage(QImage):
                     # It may happen when channel values are concentrated
                     # on the first and/or last bins.
                     return
-                h = min(h, imgH - 1) # height of rect must be < height of img, otherwise fillRect does nothing
-                rect = QRect(int((bin_edges[i] - range[0]) * scale), max(img.height() - h, 0), int((bin_edges[i + 1] - bin_edges[i]) * scale+1), h)
+                h = min(h, imgH - 1)  # height of rect must be < height of img, otherwise fillRect does nothing
+                rect = QRect(int((bin_edges[i] - range[0]) * scale), max(img.height() - h, 0),
+                             int((bin_edges[i + 1] - bin_edges[i]) * scale+1), h)
                 painter.fillRect(rect, color)
                 # clipping indicators
                 if i == 0 or i == len(hist)-1:
                     left = bin_edges[0 if i == 0 else -1]
                     if range[0] < left < range[1]:
                         continue
-                    left =  left - (10 if i > 0 else 0)
+                    left = left - (10 if i > 0 else 0)
                     percent = hist[i] * (bin_edges[i+1]-bin_edges[i])
                     if percent > clipping_threshold:
                         # calculate the color of the indicator according to percent value
@@ -159,14 +196,14 @@ class bImage(QImage):
         bufL = cv2.cvtColor(QImageBuffer(self)[:, :, :3], cv2.COLOR_BGR2GRAY)[..., np.newaxis]  # returns Y (YCrCb) : Y = 0.299*R + 0.587*G + 0.114*B
         buf = None  # TODO added 5/11/18 validate
         if mode == 'RGB':
-            buf = QImageBuffer(self)[:,:,:3][:,:,::-1]  #RGB
+            buf = QImageBuffer(self)[:, :, :3][:, :, ::-1]  # RGB
         elif mode == 'HSV':
             buf = self.getHSVBuffer()
         elif mode == 'HSpB':
             buf = self.getHspbBuffer()
         elif mode == 'Lab':
             buf = self.getLabBuffer()
-        elif mode =='Luminosity':
+        elif mode == 'Luminosity':
             chans = []
         img = QImage(size.width(), size.height(), QImage.Format_ARGB32)
         img.fill(bgColor)
@@ -179,11 +216,11 @@ class bImage(QImage):
             # even for small data size (<=250000), so we don't use it.
             # This is a numpy bug : in the module function_base.py
             # a reasonable upper bound for bins should be chosen to prevent memory error.
-            if mode=='Luminosity' or addMode=='Luminosity':
+            if mode == 'Luminosity' or addMode == 'Luminosity':
                 hist, bin_edges = np.histogram(bufL, bins=100, density=True)
                 drawChannelHistogram(qp, hist, bin_edges, Qt.gray)
             hist_L, bin_edges_L = [0]*len(chans), [0]*len(chans)
-            for i,ch in enumerate(chans):
+            for i, ch in enumerate(chans):
                 buf0 = buf[:, :, ch]
                 hist_L[i], bin_edges_L[i] = np.histogram(buf0, bins=100, density=True)
                 # to prevent artifacts, the histogram bins must be drawn
@@ -204,13 +241,16 @@ class bImage(QImage):
             qp.end()
         buf = QImageBuffer(img)
         # if len(chans) > 1, clip gray area to improve the aspect of the histogram
-        if len(chans) > 1 :
-            buf[:,:,:3] = np.where(np.min(buf, axis=-1)[:,:,np.newaxis]>=100, np.array((100,100,100))[np.newaxis, np.newaxis,:], buf[:,:,:3] )
+        if len(chans) > 1:
+            buf[:, :, :3] = np.where(np.min(buf, axis=-1)[:, :, np.newaxis] >= 100,
+                                     np.array((100, 100, 100))[np.newaxis, np.newaxis, :], buf[:, :, :3])
         return img
 
-QImageFormats = {0:'invalid', 1:'mono', 2:'monoLSB', 3:'indexed8', 4:'RGB32', 5:'ARGB32',6:'ARGB32 Premultiplied',
-                 7:'RGB16', 8:'ARGB8565 Premultiplied', 9:'RGB666',10:'ARGB6666 Premultiplied', 11:'RGB555', 12:'ARGB8555 Premultiplied',
-                 13: 'RGB888', 14:'RGB444', 15:'ARGB4444 Premultiplied'}
+
+QImageFormats = {0: 'invalid', 1: 'mono', 2: 'monoLSB', 3: 'indexed8', 4: 'RGB32', 5: 'ARGB32',6: 'ARGB32 Premultiplied',
+                 7: 'RGB16', 8: 'ARGB8565 Premultiplied', 9: 'RGB666',10: 'ARGB6666 Premultiplied', 11: 'RGB555',
+                 12: 'ARGB8555 Premultiplied', 13: 'RGB888', 14: 'RGB444', 15: 'ARGB4444 Premultiplied'}
+
 
 def ndarrayToQImage(ndimg, format=QImage.Format_ARGB32):
     """
@@ -225,15 +265,16 @@ def ndarrayToQImage(ndimg, format=QImage.Format_ARGB32):
     @rtype: QImage
     """
     if ndimg.ndim != 3 or ndimg.dtype != 'uint8':
-        raise ValueError("ndarray2QImage : array must be 3D with dtype=uint8, found ndim=%d, dtype=%s" %(ndimg.ndim, ndimg.dtype))
+        raise ValueError("ndarray2QImage : array must be 3D with dtype=uint8, found ndim=%d, dtype=%s" % (ndimg.ndim, ndimg.dtype))
     bytePerLine = ndimg.shape[1] * ndimg.shape[2]
-    if len(np.ravel(ndimg).data)!=ndimg.shape[0]*bytePerLine :  # TODO added ravel 5/11/17 needed by vImage.resize
+    if len(np.ravel(ndimg).data) != ndimg.shape[0]*bytePerLine:
         raise ValueError("ndarrayToQImage : conversion error")
     # build QImage from buffer
     qimg = QImage(ndimg.data, ndimg.shape[1], ndimg.shape[0], bytePerLine, format)
     if qimg.format() == QImage.Format_Invalid:
         raise ValueError("ndarrayToQImage : wrong conversion")
     return qimg
+
 
 def QImageBuffer(qimg):
     """
@@ -258,6 +299,6 @@ def QImageBuffer(qimg):
     # suppressing all dependencies due to implicit data sharing.
     # To avoid deep copy use constBits() instead (Caution : it returns a read-only buffer).
     ptr = qimg.bits()  # type memoryview, items are bytes : ptr.itemsize = 1
-    #convert buffer to ndarray and reshape
-    h,w = qimg.height(), qimg.width()
+    # convert buffer to ndarray and reshape
+    h, w = qimg.height(), qimg.width()
     return np.asarray(ptr, dtype=np.uint8).reshape(h, w, Bpp)  # specifying dtype is mandatory to prevent copy of data
