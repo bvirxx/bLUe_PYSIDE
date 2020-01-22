80de04e196787055921ab9b165e99e462ba44324
bvirxx <bernard.virot@libertysurf.fr>
2020-01-22 15:23:29 +0100

    Optimized computation of array expressions

--- bLUeCore/trilinear.py
+++ bLUeCore/trilinear.py
@@ -90,27 +90,46 @@ def interpTriLinear(LUT, LUTSTEP, ndImg, convert=True):
     # alpha=np.dstack((alpha, alpha, alpha))
     alpha = alpha[:, :, np.newaxis]  # broadcasting tested slower
 
-    I11Value = ndImg11 + alpha * (ndImg13 - ndImg11)  # oneMinusAlpha * ndImg11 + alpha * ndImg13
-    I12Value = ndImg10 + alpha * (ndImg12 - ndImg10)  # oneMinusAlpha * ndImg10 + alpha * ndImg12
-    I21Value = ndImg01 + alpha * (ndImg03 - ndImg01)  # oneMinusAlpha * ndImg01 + alpha * ndImg03
-    I22Value = ndImg00 + alpha * (ndImg02 - ndImg00)  # oneMinusAlpha * ndImg00 + alpha * ndImg02
+    # I11Value = ndImg11 + alpha * (ndImg13 - ndImg11)  # oneMinusAlpha * ndImg11 + alpha * ndImg13
+    # I12Value = ndImg10 + alpha * (ndImg12 - ndImg10)  # oneMinusAlpha * ndImg10 + alpha * ndImg12
+    # I21Value = ndImg01 + alpha * (ndImg03 - ndImg01)  # oneMinusAlpha * ndImg01 + alpha * ndImg03
+    # I22Value = ndImg00 + alpha * (ndImg02 - ndImg00)  # oneMinusAlpha * ndImg00 + alpha * ndImg02
+
+    # optimization of the 4 (commented out) lines above
+    # reducing the calls to memory allocation
+    def add(A, a, B, C):
+        """optimized computation of A + a *(B - C)
+            A and B are disjoint arrays"""
+        B -= C
+        B *= a
+        A += B
+        return A
+    I11Value = add(ndImg11, alpha, ndImg13, ndImg11)
+    I12Value = add(ndImg10, alpha, ndImg12, ndImg10)
+    I21Value = add(ndImg01, alpha, ndImg03, ndImg01)
+    I22Value = add(ndImg00, alpha, ndImg02, ndImg00)
+
     # allowing to free memory
-    del ndImg00, ndImg01, ndImg02, ndImg03, ndImg10, ndImg11, ndImg12, ndImg13
+    del ndImg02, ndImg03, ndImg12, ndImg13
 
     beta = ndImgF[:, :, 0] - r0
     # beta = np.dstack((beta, beta, beta))
     beta = beta[..., np.newaxis]
 
-    I1Value = I12Value + beta * (I11Value - I12Value)  # oneMinusBeta * I12Value + beta * I11Value
-    I2Value = I22Value + beta * (I21Value - I22Value)  # oneMinusBeta * I22Value + beta * I21Value
+    # I1Value = I12Value + beta * (I11Value - I12Value)  # oneMinusBeta * I12Value + beta * I11Value
+    # I2Value = I22Value + beta * (I21Value - I22Value)  # oneMinusBeta * I22Value + beta * I21Value
+    I1Value = add(I12Value, beta, I11Value, I12Value)
+    I2Value = add(I22Value, beta, I21Value, I22Value)
+
     # allowing to free memory
-    del I11Value, I12Value, I21Value, I22Value
+    del I11Value, I21Value
 
     gamma = ndImgF[:, :, 2] - b0
     # gamma = np.dstack((gamma, gamma, gamma))
     gamma = gamma[..., np.newaxis]
 
-    IValue = I2Value + gamma * (I1Value - I2Value)  # (1 - gamma) * I2Value + gamma * I1Value
+    # IValue = I2Value + gamma * (I1Value - I2Value)  # (1 - gamma) * I2Value + gamma * I1Value
+    IValue = add(I2Value, gamma, I1Value, I2Value)
 
     if convert:
         # clip in place
