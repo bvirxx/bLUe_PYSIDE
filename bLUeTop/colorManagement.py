158a437fef2b1bf7e3fcb2dddc0eaf46ce2c7295
bernard <bernard.virot@libertysurf.fr>
2020-10-12 15:29:13 +0200

    Added detection of the display color profile on Ubuntu

--- bLUeTop/colorManagement.py
+++ bLUeTop/colorManagement.py
@@ -18,13 +18,13 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import sys
 
 import numpy as np
-from PIL import Image, _imagingcms
-
+from PIL import Image
 from PIL.ImageCms import getOpenProfile, getProfileInfo, \
     buildTransformFromOpenProfiles, applyTransform, INTENT_PERCEPTUAL, ImageCmsProfile, PyCMSError, core
 from PySide2.QtGui import QImage
 
 from bLUeGui.bLUeImage import QImageBuffer
+from bLUeGui.dialog import dlgWarn
 
 from bLUeTop.settings import SRGB_PROFILE_PATH, ADOBE_RGB_PROFILE_PATH, DEFAULT_MONITOR_PROFILE_PATH
 
@@ -34,42 +34,51 @@ if sys.platform == 'win32':
 
 class icc:
     """
-    Container for color management related flags and methods.
-    Should never be instantiated
+    Container for color management related options and methods.
+    Should never be instantiated.
     """
     HAS_COLOR_MANAGE = False  # menu action "color manage" will be disabled
     COLOR_MANAGE = False  # no color management
-    defaultWorkingProfile = getOpenProfile(SRGB_PROFILE_PATH)
+    # try to find a default profile for images without a valid profile
+    try:
+        defaultWorkingProfile = getOpenProfile(SRGB_PROFILE_PATH)
+    except PyCMSError:
+        dlgWarn('No valid sRGB color profile found.\nSet SYSTEM_PROFILE_DIR in config.json', info='Invalid profile %s' % SRGB_PROFILE_PATH)
+        sys.exit()
     monitorProfile, workingProfile, workToMonTransform = (None,)*3
     workingProfileInfo, monitorProfileInfo = '', ''
 
     @staticmethod
-    def B_get_display_profile(handle=None):
+    def B_get_display_profile(handle=None, device_id=None):
         """
-        bLUe version for get_display_profile: should be
-        completed.
-        Note. The PIL function ImageCms.get_display_profile is system dependent,
-        it fails (at least) for win64.
+        bLUe version of ImageCms get_display_profile.
         @param handle: screen handle (Windows)
         @type handle: int
+        @param device_id: name of display
+        @type device_id: str
         @return: monitor profile
         @rtype: ImageCmsProfile
         """
         if sys.platform == "win32":
-            # from PIL import ImageWin
-            # if isinstance(handle, ImageWin.HDC):
-            profile = core.get_display_profile_win32(handle, 1)
-            # else:
-            # profile = core.get_display_profile_win32(handle or 0)
+            profile_path = core.get_display_profile_win32(handle, 1)
         else:
-            profile = DEFAULT_MONITOR_PROFILE_PATH
             try:
-                get = _imagingcms.get_display_profile
-            except AttributeError:
-                pass
-            else:
-                profile = get()
-        return ImageCmsProfile(profile)
+                from gi.repository import Gio, Colord
+                GIO_CANCELLABLE = Gio.Cancellable.new()
+                client = Colord.Client.new()
+                client.connect_sync(GIO_CANCELLABLE)
+                device = client.find_device_sync('xrandr-' + device_id, GIO_CANCELLABLE)
+                device.connect_sync(GIO_CANCELLABLE)
+                default_profile = device.get_default_profile()
+                default_profile.connect_sync(GIO_CANCELLABLE)
+                profile_path = default_profile.get_filename()
+            except ImportError:
+                profile_path = DEFAULT_MONITOR_PROFILE_PATH
+        try:
+            Cms_profile = ImageCmsProfile(profile_path)
+        except (OSError, TypeError):
+            Cms_profile = None
+        return Cms_profile
 
     @classmethod
     def configure(cls, qscreen=None, colorSpace=-1, workingProfile=None):
@@ -77,9 +86,9 @@ class icc:
         Try to configure color management for the monitor
         specified by QScreen, and build an image transformation
         from the working profile (default sRGB) to the monitor profile.
-        This transformation is convenient to match image colors to screen colors.
+        This transformation is convenient to map image colors to screen colors.
         @param qscreen: QScreen instance
-        @type qscreen: QScreen
+        @type qscreen: QScreep
         @param colorSpace:
         @type colorSpace
         @param workingProfile:
@@ -135,18 +144,19 @@ class icc:
         The method returns None if no profile can be found.
         @param qscreen: QScreen instance
         @type qscreen: QScreen
-        @return: monitor profile
+        @return: monitor profile or None
         @rtype: CmsProfile
         """
-        # from PIL.ImageWin import HDC
+        if qscreen is None:
+            return ImageCmsProfile(DEFAULT_MONITOR_PROFILE_PATH)
         try:
-            if qscreen is not None and sys.platform == 'win32':
+            if sys.platform == 'win32':
                 dc = win32gui.CreateDC(str(qscreen.name()), None, None)
-                monitorProfile = cls.B_get_display_profile(dc)
+                monitorProfile = cls.B_get_display_profile(handle=dc)
             else:
-                monitorProfile = cls.B_get_display_profile()
+                monitorProfile = cls.B_get_display_profile(device_id=qscreen.name())
         except (RuntimeError, OSError, TypeError):
-            monitorProfile = None
+            pass
         return monitorProfile
 
 
