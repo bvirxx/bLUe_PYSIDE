5a82a00e8ece3681061365ce42cdb5708aa1fd3b
bernard <bernard.virot@libertysurf.fr>
2020-10-15 13:29:31 +0200

    Refactoring

--- bLUeTop/colorManagement.py
+++ bLUeTop/colorManagement.py
@@ -26,10 +26,54 @@ from PySide2.QtGui import QImage
 from bLUeGui.bLUeImage import QImageBuffer
 from bLUeGui.dialog import dlgWarn
 
-from bLUeTop.settings import SRGB_PROFILE_PATH, ADOBE_RGB_PROFILE_PATH, DEFAULT_MONITOR_PROFILE_PATH
+from bLUeTop.settings import COLOR_MANAGE_OPT, SRGB_PROFILE_PATH, ADOBE_RGB_PROFILE_PATH, DEFAULT_MONITOR_PROFILE_PATH
 
-if sys.platform == 'win32':
-    import win32gui
+if COLOR_MANAGE_OPT:
+    if sys.platform == 'win32':
+        import win32gui
+    else:
+        # python-gi flag
+        HAS_GI = False
+        try:
+            from gi.repository import GLib, Gio, Colord
+            HAS_GI = True
+        except ImportError:
+            pass
+        if not HAS_GI:
+            dlgWarn("Automatic detection of monitor profile needs gi installed.\n trying to use %s instead" % DEFAULT_MONITOR_PROFILE_PATH)
+            try:
+                getOpenProfile(DEFAULT_MONITOR_PROFILE_PATH)
+            except PyCMSError:
+                dlgWarn("Invalid profile %s" % DEFAULT_MONITOR_PROFILE_PATH, info="Color management is disabled")
+
+
+def get_default_working_profile():
+    """
+    try to find a default image profile
+    @return: profile
+    @rtype: ImageCmsProfile
+    """
+    try:
+        profile = getOpenProfile(SRGB_PROFILE_PATH)
+    except PyCMSError:
+        dlgWarn('No valid sRGB color profile found.\nSet SYSTEM_PROFILE_DIR and SRGB_PROFILE_NAME in your config.json',
+                info='Invalid profile %s' % SRGB_PROFILE_PATH)
+        sys.exit()
+    return profile
+
+
+def get_default_monitor_profile():
+    """
+    try to find a default image profile
+    @return: profile or None
+    @rtype: ImageCmsProfile or None
+    """
+    profile = None
+    try:
+        profile = getOpenProfile(DEFAULT_MONITOR_PROFILE_PATH)
+    except PyCMSError:
+        pass
+    return profile
 
 
 class icc:
@@ -39,12 +83,7 @@ class icc:
     """
     HAS_COLOR_MANAGE = False  # menu action "color manage" will be disabled
     COLOR_MANAGE = False  # no color management
-    # try to find a default profile for images without a valid profile
-    try:
-        defaultWorkingProfile = getOpenProfile(SRGB_PROFILE_PATH)
-    except PyCMSError:
-        dlgWarn('No valid sRGB color profile found.\nSet SYSTEM_PROFILE_DIR in config.json', info='Invalid profile %s' % SRGB_PROFILE_PATH)
-        sys.exit()
+
     monitorProfile, workingProfile, workToMonTransform = (None,)*3
     workingProfileInfo, monitorProfileInfo = '', ''
 
@@ -56,14 +95,15 @@ class icc:
         @type handle: int
         @param device_id: name of display
         @type device_id: str
-        @return: monitor profile
-        @rtype: ImageCmsProfile
+        @return: monitor profile or None
+        @rtype: ImageCmsProfile or None
         """
+        profile_path = DEFAULT_MONITOR_PROFILE_PATH
         if sys.platform == "win32":
             profile_path = core.get_display_profile_win32(handle, 1)
-        else:
+        elif HAS_GI:
             try:
-                from gi.repository import Gio, Colord
+                from PySide2.QtWidgets import QApplication, QMainWindow
                 GIO_CANCELLABLE = Gio.Cancellable.new()
                 client = Colord.Client.new()
                 client.connect_sync(GIO_CANCELLABLE)
@@ -72,13 +112,39 @@ class icc:
                 default_profile = device.get_default_profile()
                 default_profile.connect_sync(GIO_CANCELLABLE)
                 profile_path = default_profile.get_filename()
-            except ImportError:
-                profile_path = DEFAULT_MONITOR_PROFILE_PATH
+            except (NameError, ImportError, GLib.GError) as e:
+                from bLUeTop.QtGui1 import window
+                dlgWarn('Cannot detect monitor profile', info=str(e), parent=window)
         try:
-            Cms_profile = ImageCmsProfile(profile_path)
-        except (OSError, TypeError):
-            Cms_profile = None
+            Cms_profile = getOpenProfile(profile_path)
+        except PyCMSError:
+            Cms_profile = get_default_monitor_profile()
         return Cms_profile
+
+    @classmethod
+    def getMonitorProfile(cls, qscreen=None):
+        """
+        Try to retrieve the default color profile
+        associated to the monitor specified by QScreen
+        (the system main display if qscreen is None).
+        The method returns None if no profile can be found.
+        @param qscreen: QScreen instance
+        @type qscreen: QScreen
+        @return: monitor profile or None
+        @rtype: CmsProfile or None
+        """
+        monitorProfile = None
+        # detecting profile
+        if qscreen is not None:
+            try:
+                if sys.platform == 'win32':
+                    dc = win32gui.CreateDC(str(qscreen.name()), None, None)
+                    monitorProfile = cls.B_get_display_profile(handle=dc)
+                else:
+                    monitorProfile = cls.B_get_display_profile(device_id=qscreen.name())
+            except (RuntimeError, OSError, TypeError):
+                pass
+        return monitorProfile
 
     @classmethod
     def configure(cls, qscreen=None, colorSpace=-1, workingProfile=None):
@@ -95,7 +161,13 @@ class icc:
         @type workingProfile:
         """
         cls.HAS_COLOR_MANAGE = False
-        # look for valid profiles
+        # a (default) working image profile is always needed, at least for RGB<-->XYZ conversions
+        cls.defaultWorkingProfile = get_default_working_profile()
+        cls.workingProfile = cls.defaultWorkingProfile
+        cls.workingProfileInfo = getProfileInfo(cls.workingProfile)
+        if not COLOR_MANAGE_OPT:
+            return
+        # looking for valid profiles
         try:
             # get monitor profile as CmsProfile object.
             if qscreen is not None:
@@ -134,31 +206,6 @@ class icc:
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
-
-    @classmethod
-    def getMonitorProfile(cls, qscreen=None):
-        """
-        Try to retrieve the default color profile
-        associated to the monitor specified by QScreen
-        (the system main display if qscreen is None).
-        The method returns None if no profile can be found.
-        @param qscreen: QScreen instance
-        @type qscreen: QScreen
-        @return: monitor profile or None
-        @rtype: CmsProfile
-        """
-        if qscreen is None:
-            return ImageCmsProfile(DEFAULT_MONITOR_PROFILE_PATH)
-        try:
-            if sys.platform == 'win32':
-                dc = win32gui.CreateDC(str(qscreen.name()), None, None)
-                monitorProfile = cls.B_get_display_profile(handle=dc)
-            else:
-                monitorProfile = cls.B_get_display_profile(device_id=qscreen.name())
-        except (RuntimeError, OSError, TypeError):
-            pass
-        return monitorProfile
-
 
 def cmsConvertQImage(image, cmsTransformation=None):
     """
