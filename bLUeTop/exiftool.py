3f6a0f4b5f6bb3fe517d4ff1a862f8b73a1b5592
bvirxx <bernard.virot@libertysurf.fr>
2022-01-03 16:08:09 +0100

    A bug corrected in get_thumbNail

--- bLUeTop/exiftool.py
+++ bLUeTop/exiftool.py
@@ -238,7 +238,7 @@ class ExifTool(object):
         @rtype: QImage
         """
         thumbnail = self.readBinaryData(f, tagname=thumbname)
-        return QImage.fromData(QByteArray.fromRawData(thumbnail), 'JPG')  # Pyside2 fromRawData takes 1 arg only
+        return QImage.fromData(QByteArray(thumbnail), 'JPG')
 
     def writeThumbnail(self, filename, thumbfile):
         """
