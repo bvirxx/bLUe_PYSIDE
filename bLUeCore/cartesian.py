c157fe7e1f06bc7644b6dc84c411fb975a88c213
bvirxx <bernard.virot@libertysurf.fr>
2025-04-28 12:24:43 +0200

    refactored

--- bLUeCore/cartesian.py
+++ bLUeCore/cartesian.py
@@ -2,43 +2,50 @@
 This file is part of bLUe software.
 """
 
-# the code below is taken from the site https://gist.github.com/hernamesbarbara/68d073f551565de02ac5
-# We gratefully acknowledge the contribution of the author.
-
 import numpy as np
 
 
-def cartesianProduct(arrayList, out=None):
+def cartesianProduct(arrays):
     """
     Builds the cartesian product of
     N one dimensional array-like objects as a numpy array
-    with shape (a1.shape,...,aN.shape, N). All arrays must have identical
-    dtypes, otherwise a ValueError exception is raised.
+    with shape (a_1.shape,...,a_N.shape, N).
+    All arrays must have the same dtype.
 
-    :param arrayList : list or tuple of 1-D array-like objects, with identical dtype
-    :type arrayList: list or tuple
-    :param out : used only for recursive calls
-    :return: the cartesian product of the arrays
+    :param arrays : list or tuple of 1-D array-like objects, with identical dtype
+    :type arrays: list or tuple of 1-D array-like objects
+    :return: ndarray representing the Cartesian product
     :rtype: ndarray
-    :raise ValueError
+    :raises ValueError: if input arrays do not share the same dtype
     """
-    arrayList = [np.asarray(x) for x in arrayList]
-    n = np.prod([x.size for x in arrayList])
-    # empty product
-    if n == 0:
+    if not arrays:
         return np.array([])
-    itemSize = len(arrayList)
-    dtype = arrayList[0].dtype
-    for a in arrayList[1:]:
-        if a.dtype != dtype:
-            raise ValueError("cartesianProduct : all arrays must have the same dtype")
-    m = n // arrayList[0].size
-    if out is None:
-        out = np.zeros([n, itemSize], dtype=dtype)
-    out[:, 0] = np.repeat(arrayList[0], m)
-    if arrayList[1:]:
-        cartesianProduct(arrayList[1:], out=out[0:m, 1:])
-        for j in range(1, arrayList[0].size):
-            out[j * m:(j + 1) * m, 1:] = out[0:m, 1:]
-    dims = tuple([x.size for x in arrayList] + [len(arrayList)])
-    return np.reshape(out, dims)
+    arrays = [np.asarray(arr) for arr in arrays]
+    dtype = arrays[0].dtype
+    if any(arr.dtype != dtype for arr in arrays):
+        raise ValueError("cartesianProduct : all arrays must have the same dtype")
+
+    def recursive_cartesian(arrays, out=None):
+        total_combinations = np.prod([arr.size for arr in arrays])
+        if total_combinations == 0:
+            return np.array([])
+
+        if out is None:  # unique allocation of the whole array
+            # all axes but the last are flattened
+            out = np.zeros((total_combinations, len(arrays)), dtype=dtype)
+
+        repeat_factor = total_combinations // arrays[0].size
+        # copy 1st array
+        out[:, 0] = np.repeat(arrays[0], repeat_factor)
+
+        if len(arrays) > 1:
+            # copy remaining arrays
+            recursive_cartesian(arrays[1:], out=out[0:repeat_factor, 1:])
+            for i in range(1, arrays[0].size):
+                out[i * repeat_factor:(i + 1) * repeat_factor, 1:] = out[0:repeat_factor, 1:]
+
+        return out
+
+    result = recursive_cartesian(arrays)
+    final_shape = tuple(arr.size for arr in arrays) + (len(arrays),)
+    return result.reshape(final_shape)
